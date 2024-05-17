0f787e9e9dacc7a9bd0460ce49d8746bb102b34a
MikeQuezada1 <st2748@utr.edu.mx>
2024-05-17 16:22:12 -0600

    changes x2

--- src/auth/[...nextauth]/route.ts
+++ src/auth/[...nextauth]/route.ts
@@ -1,16 +1,17 @@
+import NextAuth from 'next-auth';
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-        authorization: {
-          params: {
-            prompt: "consent",
-            access_type: "offline",
-            response_type: "code",
-          },
+  providers: [
+    Googleprovider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
         },
-      }),
-    ],
-  })
+      },
+    }),
+  ],
+});
